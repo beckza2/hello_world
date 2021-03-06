dd148f479a8a3bfa826d59aedec34d8e721f7e91
Rnnasak Chunhachokkul <80138030+beckza2@users.noreply.github.com>
2021-03-06 14:48:15 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        drawer: Drawer(
+          child: Text("hello drawer"),
+        ),
+        appBar: AppBar(
+          title: Text("My first Project"),
+        ),
+        body: Center(
+          child: Text("hello world"),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.phone), label: "Phone")
+          ],
+        ),
+      ),
+    );
+  }
+}
